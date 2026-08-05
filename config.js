b6a64f6b188de32b5fbebd0fd24cc303c9c715dd
datamranti <data@mranti.my>
2026-08-05 17:09:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,20 @@ window.MRANTI_AI_BRAIN_CONFIG = {
     health: "mranti-ai-brain-health"
   },
 
-  demoMode: false,
+  auth: {
+    allowedDomain: "mranti.my",
+    driveScope: "https://www.googleapis.com/auth/drive.readonly",
 
+    firebase: {
+      apiKey: "AIzaSyDXz0gL67r9BafTg8nL6VdHYdw1tfcxhto",
+      authDomain: "mranticrm.firebaseapp.com",
+      projectId: "mranticrm",
+      storageBucket: "mranticrm.firebasestorage.app",
+      messagingSenderId: "314966643466",
+      appId: "1:314966643466:web:aff9ad95262d1df8f960f3"
+    }
+  },
+
+  demoMode: false,
   requestTimeoutMs: 90000
 };
